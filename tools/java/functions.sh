9afff680059eb7be14eb95c3b444b936b2c939d6
rootid <vsinhsawant@gmail.com>
2019-08-13 18:25:50 -0400

    updated java functions

--- tools/java/functions.sh
+++ tools/java/functions.sh
@@ -1,4 +1,12 @@
-# Locate specific dependecy for given input eg. mdi velocity:velocity  
+# Locate specific dependecy for given input eg. mdi velocity:velocity
 function mdi() {
   mvn dependency:tree -Dverbose -Dincludes=$1
+}
+
+# list the keystore/truststore to view certificate in the keystore
+# keystore - Store private key entries, certificates with public keys or just secret keys
+# truststore - Client look up the associated certificate in our truststore
+# eg. keytool_list $file_path
+function keytool_list() {
+  keytool -v -list -keystore $1
 }
