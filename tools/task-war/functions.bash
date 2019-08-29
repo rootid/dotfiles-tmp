04786d1598a1c4842af42a388ac06d69655e14aa
rootid <vsinhsawant@gmail.com>
2019-08-28 21:38:13 -0400

    Updated functions

--- tools/task-war/functions.bash
+++ tools/task-war/functions.bash
@@ -4,3 +4,32 @@ function go_task_list() {
   task list
 }
 
+# List daily completed tasks
+function go_task_status() {
+  task dailystatus
+}
+
+# go_task_add "Mt. Rainier National Park project:next_task.hiking"
+function go_task_add() {
+  task add $1
+}
+# Start task id
+function go_task_start() {
+  task $1 start
+}
+
+# go_task_done <task_id>
+function go_task_done() {
+  task $1 done
+}
+
+# Display daily burndown report
+function go_task_show() {
+  task burndown
+}
+
+# Display daily burndown report
+function go_task_show_monthly() {
+  task burndown_monthly
+}
+
