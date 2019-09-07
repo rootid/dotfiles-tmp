bff10cbd9cb63c7e8fccdd062907a2cf49b0f817
rootid <vsinhsawant@gmail.com>
2019-09-06 21:30:19 -0400

    Updated block dns script

--- utils/focus.sh
+++ utils/focus.sh
@@ -1,6 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-dns_file=$HOME/.block-dns
+# Set up the cron job
+# sudo crontab -e
+# */5 22-23,0-4 * * * $HOME/dotfiles/utils/focus.sh $HOME/.block-dns
+# 0 7 * * * $HOME/dotfiles/utils/pause_focus.sh
+
+
+dns_file=$1
+
+if [ -z ${dns_file} ]
+then
+  dns_file=$HOME/.block-dns
+fi
+if [ ! -f ${dns_file} ]
+then
+  echo "Enter focus.sh <valid_dns_config_file>"
+  exit 1
+fi
 
 while read line
 do
