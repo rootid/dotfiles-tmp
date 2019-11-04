05cf7344b58355e039a449beef0c63109ea89f1e
rootid <vsinhsawant@gmail.com>
2019-11-04 07:22:58 -0500

    Added bookmarks

--- tools/buku/functions.bash
+++ tools/buku/functions.bash
@@ -25,6 +25,71 @@ function go_bk_todo() {
   buku -a $1 $2  --title todo
 }
 
+# Add video
+function go_bk_video() {
+  buku -a $1 $2 --title video
+}
+
+# Add system_design
+function go_bk_sys_design() {
+  buku -a $1 $2 --title system_design
+}
+
+# Add algo
+function go_bk_algo() {
+  buku -a $1 $2 --title algo
+}
+
+# Add paper
+function go_bk_paper() {
+  buku -a $1 $2 --title paper
+}
+
+# Add book
+function go_bk_book() {
+  buku -a $1 $2 --title book
+}
+
+# Add java
+function go_bk_java() {
+  buku -a $1 $2 --title java
+}
+
+# Add python
+function go_bk_python() {
+  buku -a $1 $2 --title python
+}
+
+# Add english
+function go_bk_english() {
+  buku -a $1 $2 --title english
+}
+
+# Add Behavioral
+function go_bk_behavioral() {
+  buku -a $1 $2 --title intv_behavioral
+}
+
+# Add paypal
+function go_bk_pp() {
+  buku -a $1 $2 --title pp_work
+}
+
+# Add video
+function go_bk_pp_video() {
+  buku -a $1 $2 --title pp_video
+}
+
+# Add video
+function go_bk_pp_lms() {
+  buku -a $1 $2 --title pp_lms
+}
+
+# Open bookmark
+function go_bk_open() {
+  buku -o $1
+}
+
 # Delete bookmark
 function go_bk_del() {
   buku --delete $1
